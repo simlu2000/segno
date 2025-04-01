d68b1159921a94bfb42d1f68f29cda8f069d4060
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2025-04-01 11:40:20 +0200

    logo area

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Segno - App Note</title>
     <style>
-         body {
+        body {
             font-family: 'Quicksand', sans-serif;
             margin: 0;
             padding: 0;
@@ -25,10 +25,11 @@ session_start();
         .content {
             flex: 1;
             display: flex;
-            flex-direction: column;
-            justify-content: center;
+            flex-direction: row;
+            justify-content: space-around;
             align-items: center;
             padding: 20px;
+            margin: 0 5%;
         }
 
         .content h1 {
@@ -44,12 +45,31 @@ session_start();
             text-align: center;
         }
 
+        #logo{
+            border-radius:50px;
+        }
+
         footer {
             text-align: center;
             padding: 10px 0;
-            background-color: rgba(255, 255, 255, 0.8); 
+            background-color: rgba(255, 255, 255, 0.8);
             font-size: 0.9rem;
             color: #777;
+        }
+        
+        #logo-area{
+            background-color: rgba(255, 255, 255, 0.5);
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            border-radius: 50px;
+        }
+
+        @media (max-width: 491px) {
+            .content {
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                margin: 0;
+            }
         }
     </style>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600&display=swap" rel="stylesheet">
@@ -57,14 +77,19 @@ session_start();
 </head>
 
 <body>
-    <?php include "./navbar.php"?>
+    <?php include "./navbar.php" ?>
 
     <div class="content">
-        <h1>Benvenuto in Segno</h1>
-        <p>Segno è una semplice app per prendere appunti. Puoi usarla per annotare idee o scrivere promemoria. Per iniziare, registrati o accedi.</p>
+        <div>
+            <h1>Benvenuto in Segno</h1>
+            <p>Segno è una semplice app per prendere appunti. Puoi usarla per annotare idee o scrivere promemoria. Per iniziare, registrati o accedi.</p>
+        </div>
+        <div id="logo-area">
+            <img id="logo" src="images/segno.png">
+        </div>
     </div>
 
-   <?php include "./footer.php"?>
+    <?php include "./footer.php" ?>
 </body>
 
 </html>
